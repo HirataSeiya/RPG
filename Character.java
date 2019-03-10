83610a9228652a815a01172bf9d93af9c074aa8a
HirataSeiya <47455456+HirataSeiya@users.noreply.github.com>
2019-03-10 20:36:12 +0900

    Add files via upload

--- Character.java
+++ Character.java
@@ -1,14 +1,42 @@
 package Method_task;
 
-public class Character {
-	public int hp,mp;
+public class Character{
 	public String name;
+	public int hp,mp;
+	public int power;
+
+
+	public Character() { //初期値
+		name = "名無し";
+		hp = 1;
+		mp = 1;
+		power = 1;
+	}
+
+	public Character(String name, int hp, int mp ,int power) {
+		this.name = name;
+		this.hp = hp;
+		this.mp = mp;
+		this.power = power;
+	}
 
 	public void Introduce(){
 		System.out.println("自己紹介をします。");
 		System.out.println("私の名前は"+name+"です。");
-		System.out.println("体力は"+hp+"です。");
-		System.out.println("魔力は"+mp+"です。");
+		System.out.println("体力は"+hp+"です,魔力は"+mp+"です。");
+		System.out.println("攻撃力は"+power+"です!");
 		System.out.println("");
 	}
+
+	public void damage(String name,int power) {
+		System.out.println(name+"は"+power+"のダメージを受けた!");
+		this.hp -= power;
+		System.out.println(this.name+"の体力は残り"+this.hp+"だ!");
+	}
+
+	public void attack(String name) {
+		System.out.println(name+"の攻撃");
+	}
+
+
 }
--- Profile.java
+++ Profile.java
@@ -3,22 +3,18 @@ package Method_task;
 public class Profile {
 
 	public static void main(String[] args) {
-		Character c1;
-		c1 = new Character();
+		Character c1 = new Character("アルス",100,50,10);
+		Character c2 = new Character("クラウド",50,100,25);
 
-		c1.name = "アルス";
-		c1.hp = 100;
-		c1.mp = 50;
-
-		Character c2;
-		c2 = new Character();
-
-		c2.name = "クラウド";
-		c2.hp = 50;
-		c2.mp = 100;
+		String target = c1.name;
 
 		c1.Introduce();
 		c2.Introduce();
+
+		System.out.println("--------------------------");
+
+		c1.attack(c1.name);
+		c2.damage(c2.name,c1.power);
 	}
 
 }
