d0b15b25c18c2de8d4c8a2b186257c2461a51eed
HirataSeiya <47455456+HirataSeiya@users.noreply.github.com>
2019-03-24 22:07:12 +0900

    Add files via upload

--- Profile.java
+++ Profile.java
@@ -3,8 +3,6 @@ package Method_task;
 public class Profile {
 
 	public static void main(String[] args) {
-//		Character aruth = new Knight("アルス",100,50,30);
-//		Character cloud = new Magician("クラウド",50,100,25);
 
 		int m1 = 2;
 		int m2 = 2;
@@ -17,14 +15,13 @@ public class Profile {
 		party2[0] = new Magician("クラウド",50,70,25);
 		party2[1] = new Knight("スコール",100,50,70);
 
-//	    int i = 0;
-	    for(int i=0;i<2;i++) {
+	    for(int i=0;i<m1;i++) {
 	    	party1[i].Introduce();
 			party1[i].attack(party2[i]);
 			System.out.println("--------------------------");
 		}
 
-		for(int i=0;i<2;i++) {
+		for(int i=0;i<m2;i++) {
 			party2[i].Introduce();
 			party2[i].attack(party1[i]);
 			System.out.println("--------------------------");
