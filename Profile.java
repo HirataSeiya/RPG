3f95b86c68cc8935b47ee1c721874d58e5324d74
HirataSeiya <47455456+HirataSeiya@users.noreply.github.com>
2019-03-24 22:04:25 +0900

    Add files via upload

--- Profile.java
+++ Profile.java
@@ -11,20 +11,20 @@ public class Profile {
 
 		Character party1[] = new Character[m1];
 		party1[0] = new Knight("アルス",100,50,30);
-		party1[1] = new Magician("アリーナ",70,40,50);
+		party1[1] = new Magician("アリーナ",70,80,50);
 
 		Character party2[] = new Character[m2];
-		party2[0] = new Magician("クラウド",50,100,25);
-		party2[1] = new Knight("スコール",40,50,70);
+		party2[0] = new Magician("クラウド",50,70,25);
+		party2[1] = new Knight("スコール",100,50,70);
 
-	    int i = 0;
-	    for(i=0;i<=m1;i++) {
-		party1[i].Introduce();
+//	    int i = 0;
+	    for(int i=0;i<2;i++) {
+	    	party1[i].Introduce();
 			party1[i].attack(party2[i]);
 			System.out.println("--------------------------");
 		}
 
-		for(i=0;i<=m1;i++) {
+		for(int i=0;i<2;i++) {
 			party2[i].Introduce();
 			party2[i].attack(party1[i]);
 			System.out.println("--------------------------");
