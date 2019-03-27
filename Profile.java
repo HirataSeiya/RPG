e0697655d12e2cab785f423fa1c200a596b8655b
HirataSeiya <47455456+HirataSeiya@users.noreply.github.com>
2019-03-27 19:06:54 +0900

    Add files via upload

--- Profile.java
+++ Profile.java
@@ -16,13 +16,13 @@ public class Profile {
 		party2[1] = new Knight("スコール",100,50,70);
 
 	    for(int i=0;i<m1;i++) {
-	    	party1[i].Introduce();
+	    	party1[i].introduce();
 			party1[i].attack(party2[i]);
 			System.out.println("--------------------------");
 		}
 
 		for(int i=0;i<m2;i++) {
-			party2[i].Introduce();
+			party2[i].introduce();
 			party2[i].attack(party1[i]);
 			System.out.println("--------------------------");
 		}
